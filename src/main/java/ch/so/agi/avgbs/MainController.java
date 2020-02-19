3ffd209295d38f2b9428495dd3bc84c4ca9aa720
Stefan Ziegler <edi.gonzales@gmail.com>
2020-02-19 21:12:04 +0100

    logout not working...

--- src/main/java/ch/so/agi/avgbs/MainController.java
+++ src/main/java/ch/so/agi/avgbs/MainController.java
@@ -1,11 +1,27 @@
 package ch.so.agi.avgbs;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.core.OAuth2AccessToken;
 
 @Controller
 public class MainController {
+
     @RequestMapping(value = "/foo", method = RequestMethod.GET)
     public String index2() {
         return "index2";
@@ -21,5 +37,8 @@ public class MainController {
     public String upload() {
         return "upload";
     }    
+
+
+
 
 }
